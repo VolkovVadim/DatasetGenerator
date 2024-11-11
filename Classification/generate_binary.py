39ba693297b6c1680960d2940cc9773f96dbbef2
Volkov Vadim <VolkovVadim5@gmail.com>
2024-11-11 18:24:20 +0300

    Added noise feature to binary classification generator

--- Classification/generate_binary.py
+++ Classification/generate_binary.py
@@ -4,8 +4,10 @@ import matplotlib.pyplot as plt
 import argparse
 
 
-EXAMPLES_COUNT     = 5000
-FUNC_TYPE          = 3
+EXAMPLES_COUNT     = 5000       # Points count in dataset
+FUNC_TYPE          = 3          # Dataset type
+NOISE_ALPHA        = 0.8        # Part of the dataset with outliers
+NOISE_LEVEL        = 0.9        # Data noise level
 
 
 def show_info(dataframe: pd.DataFrame) -> None:
@@ -27,6 +29,7 @@ def show_info(dataframe: pd.DataFrame) -> None:
 
 def visualize(data: pd.DataFrame) -> None:
     plt.style.use('ggplot')
+    plt.figure(figsize=(12, 10), dpi=80)
 
     label_font = {
         'family': 'serif',
@@ -52,24 +55,37 @@ def visualize(data: pd.DataFrame) -> None:
     plt.show()
 
 
-def generate(func_type: int = 1) -> pd.DataFrame:
+def generate(
+        count: int, 
+        func_type: int = 1, 
+        noise: bool = False,
+        noise_alpha: float = 0.1,
+        noise_level: float = 1.0
+) -> pd.DataFrame:
     if func_type < 1 or func_type > 3:
         func_type = 1
 
     min_f1, max_f1 = -25.0, 25.0
     min_f2, max_f2 = -25.0, 25.0
 
-    F1 = np.random.uniform(min_f1, max_f1, size=(EXAMPLES_COUNT,))
-    F2 = np.random.uniform(min_f2, max_f2, size=(EXAMPLES_COUNT,))
+    F1 = np.random.uniform(min_f1, max_f1, size=(count,))
+    F2 = np.random.uniform(min_f2, max_f2, size=(count,))
 
     if func_type == 1:
-        Y = [1 if F1[i] - F2[i] > 0 else 0 for i in range(EXAMPLES_COUNT)]
+        Y = [1 if F1[i] - F2[i] > 0 else 0 for i in range(count)]
 
     if func_type == 2:
-        Y = [1 if F1[i] - 10.0 * np.sin(0.25 * F2[i]) > 0 else 0 for i in range(EXAMPLES_COUNT)]
+        Y = [1 if F1[i] - 10.0 * np.sin(0.25 * F2[i]) > 0 else 0 for i in range(count)]
 
     if func_type == 3:
-        Y = [1 if F1[i] - (F2[i] + 5.5 * np.sin(0.5 * F2[i])) > 0 else 0 for i in range(EXAMPLES_COUNT)]
+        Y = [1 if F1[i] - (F2[i] + 5.5 * np.sin(0.5 * F2[i])) > 0 else 0 for i in range(count)]
+
+    if noise:
+        examples_with_noise = int(float(count) * noise_alpha)
+        selected_examples = np.random.randint(count, size=examples_with_noise)
+        for i in selected_examples:
+            F1[i] += np.random.normal(scale=noise_level)
+            F2[i] += np.random.normal(scale=noise_level)
 
     data = {
         "feature_1": F1,
@@ -88,6 +104,7 @@ if __name__ == "__main__":
     # Parse command args
     parser = argparse.ArgumentParser()
     parser.add_argument("-s", "--save", help="save genereated dataset to file", action="store_true")
+    parser.add_argument("-n", "--noise", help="generate dataset with outliers", action="store_true")
     parser.add_argument("-c", "--count", help="dataset points count", type=int)
     parser.add_argument("-t", "--type", help="generator function type", type=int)
 
@@ -100,13 +117,25 @@ if __name__ == "__main__":
         FUNC_TYPE = args.type
 
     # Generate data
-    df_generated_dataset = generate(FUNC_TYPE)
+    is_noised = True if args.noise else False
+    df_generated_dataset = generate(
+        EXAMPLES_COUNT, 
+        func_type=FUNC_TYPE,
+        noise=is_noised,
+        noise_alpha=NOISE_ALPHA,
+        noise_level=NOISE_LEVEL
+    )
     show_info(df_generated_dataset)
     visualize(df_generated_dataset)
 
     # Save data to file
     if args.save:
-        dataset_filename = f"binary_classification_v{FUNC_TYPE}_{EXAMPLES_COUNT}.csv"
+        file_format = "csv"
+        dataset_filename = f"binary_classification_v{FUNC_TYPE}_{EXAMPLES_COUNT}"
+        if is_noised:
+            dataset_filename += "_with_noise"
+        dataset_filename += f".{file_format}"
+
         df_generated_dataset.to_csv(dataset_filename, index=False)
         print(f"Dataset saved to file : {dataset_filename}")
 
